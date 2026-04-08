42811f962f8b6219485f8aae41e390b1a787e583
ANova0 <ANova0@users.noreply.github.com>
2026-04-09 00:51:27 +0300

    chore: exclude app/build from git

--- app/build/generated/hilt/component_trees/coreDebug/dagger/hilt/internal/processedrootsentinel/codegen/_com_stukachoff_StukachoffApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.stukachoff.StukachoffApp"
-)
-public final class _com_stukachoff_StukachoffApp {
-}
